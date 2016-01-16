ee9bc7f372989ad537d5ccef4932d0687caa3dea
Andi <andicbuchanan@gmail.com>
2016-01-17 10:58:02 +1300

    tweaked order of script loading

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
+window.onload = function(){
+  var myImage = document.querySelector('img');
+  myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'me.jpg') {
       myImage.setAttribute ('src','otherme.jpg');
-    } else {
-      myImage.setAttribute ('src','me.jpg');
-    }
+      } else {
+        myImage.setAttribute ('src','me.jpg');
+      }
+  }
 }
